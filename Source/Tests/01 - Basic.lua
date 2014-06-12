08006060d10572c078119d94f2c8bde1b9d79e94
Joël R. Langlois <joel.r.langlois@gmail.com>
2014-06-12 14:01:31 -0400

    Initial commit.

---
+++ Source/Tests/01 - Basic.lua
@@ -0,0 +1,7 @@
+if "a" == "b" then
+    print ("If this prints, we have a problem!")
+elseif 7 > 3 then
+    print ("Lua appears to have done elementary math.")
+else
+    print ("Lua didn't go to school, it would seem.")
+end
